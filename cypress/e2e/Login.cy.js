1d48b4c7b795f9afed61745981ec60008c0f1204
Olusoga Daniel <Olusogadaniel001@gmail.com>
2022-11-19 12:54:24 +0100

    cypress Automation

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,17 @@
+//  <refrence types ="cypress"/>  //to refrence   cypress auto complete
+
+
+ it('Mtn Biosmart Login', function(){
+
+    //  cy.visit('https://biowebstaging.mtnnigeria.net/')
+    
+        cy.visit('https://simregpoc.mtnnigeria.net')
+     cy.get('#loginUsername').type('vadewumi@seamfix.com')
+     cy.get('#loginPassword').type('Bankole123456!!!')
+
+     cy.get('#loginSubmitBtn > .ng-star-inserted').click()
+     cy.get('#menuSidenav > .mat-button-wrapper > .mat-icon').click()
+     cy.get('#camera > .mat-list-item-content > .mat-list-text > .mat-line').click()
+     cy.get('#ccGotoNewRegMsisdnDiv > .ng-star-inserted').click()
+
+ } );
---
+++ cypress/e2e/google.cy.js
@@ -0,0 +1,10 @@
+it('Google Test', function(){
+
+        cy.visit('https://Google.com')
+        cy.get('.gLFyf').type('Automation Step by Step{enter}')
+        cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l', {timeout: 6000}).click()
+        
+        
+        
+})
+
