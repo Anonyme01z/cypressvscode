322477e9cba66cd4215dc54f1b3b5937ca5a5ea0
Olusoga Daniel <Olusogadaniel001@gmail.com>
2022-11-19 15:06:53 +0100

    Assertions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
+// e2e: {
+//   setupNodeEvents(on, config) {
+//     // implement node event listeners here
+//   },
+// },
