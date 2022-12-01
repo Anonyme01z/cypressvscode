e1f434ea55deb0899e8310e4d83be16c1fea9734
Olusoga Daniel <Olusogadaniel001@gmail.com>
2022-12-01 15:24:45 +0100

    Updated Code

--- cypress/e2e/NewregMsisdn.cy.js
+++ cypress/e2e/NewregMsisdn.cy.js
@@ -1,4 +1,4 @@
-//  <refrence types ="cypress"/>  //to refrence   cypress auto complete
+//   <refrence types ="cypress"/>  //to refrence   cypress auto complete
 
 const { isExportDeclaration, isAssertClause } = require("typescript");
 
@@ -31,20 +31,53 @@ const { isExportDeclaration, isAssertClause } = require("typescript");
       cy.get('#pcSavePortraitButton').click()
       cy.get('#nrmProccedToCapture > .mat-button-wrapper > .text-white').click()
 
-      cy.get('#mat-select-value-17 > .mat-select-placeholder').click()
-      cy.get('#mat-option-27 > .mat-option-text').click()
-      cy.get('#searchParameterField').type('90108320911')
-      cy.get('#ninSeachAndCaptureButton').click()
+      cy.get('#mat-select-value-15 > .mat-select-placeholder').click()
+      cy.get('#mat-option-22 > .mat-option-text').click()
+      
+       cy.get('#searchParameterField').type('90108320911')
+       cy.get('#ninSeachAndCaptureButton').click()
 
-      //Demo Page
+       //Demo Page
+       cy.get('#dcDMotherMaiden').type('Josephine')
+       cy.get('#dcDHouseAddress').type('980')
+       cy.get('#dcDStreetAddress').type('Unity')
+       cy.get('#dcDCityAddress').type('Ikeja')
+       cy.get('#dcDAlternatePhone').type('08107144826')
 
-      cy.get('#dcDMotherMaiden').type('Josephine')
-      cy.get('#dcDHouseAddress').type('980')
-      cy.get('#dcDStreetAddress').type('Unity')
-      cy.get('#dcDCityAddress').type('Ikeja')
-      cy.get('.mat-form-field.ng-tns-c68-52 > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type('08104578293')
+   
+                              
 
-      cy.get('#dcDStateofReg > .ng-select-container > .ng-value-container > .ng-input > input').click
-      cy.get('#a51d3c6b0df6-3').click()
+                               //Mode of ID
+                               cy.get('#mat-select-value-21 > .mat-select-placeholder').click()
+                               cy.get('#mat-option-29 > .mat-option-text').click()
 
-} );
+                               //State&Lga ofOrigin
+                                cy.get('#dcDStateOfOrigin > .ng-select-container > .ng-value-container > .ng-input > input').type('Zamfara')
+                                cy.get('.ng-option-label').click()
+
+                                cy.get('#dcDLgaOfOrigin > .ng-select-container > .ng-value-container > .ng-input > input').type('Zurmi')
+                                cy.get('.ng-option-label').click()
+
+                               //State&LgaOfReg
+
+                               cy.get('#dcDStateofReg > .ng-select-container > .ng-value-container > .ng-input > input').type('Abia')
+                           
+
+                               cy.get('.ng-option-label').click()
+
+                               cy.get('#dcDLgaOfReg > .ng-select-container > .ng-value-container > .ng-input > input').type('Bende')
+                            
+                              cy.get('.ng-option-label').click()
+
+                               //State,Lga&AreaofResi
+                               cy.get('#dcDStateOfResidence > .ng-select-container > .ng-value-container > .ng-input > input').type('Abia')
+                              
+                              cy.get('.ng-option-label').click()
+
+                               cy.get('#adaeeaef5c19-2 > .ng-option-label').type('Aba North')
+                              cy.get('.ng-option-label').click()
+
+                               cy.get('#dcDAreaOfResidence > .ng-select-container > .ng-value-container > .ng-input > input').click()
+                               cy.get('#ab9a23bcc059-1 > .ng-option-label').click()
+
+})
